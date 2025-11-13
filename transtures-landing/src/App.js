a06a108a75a559ab4b3357567f6bf13cf12c4a02
Orlando Calvo <ocalvo@amare.com>
2025-11-13 02:12:11 -0600

    fix: admin panel nested routing

--- transtures-landing/src/App.js
+++ transtures-landing/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/*" element={<Admin />} />
       </Routes>
     </Router>
   );
